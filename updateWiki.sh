4dafd621eb99419d3bd95e09a2087e7e54a457bb
YongHao Hu <yonghao.hu@grabtaxi.com>
2021-12-23 23:10:10 +0800

    new wiki

--- updateWiki.sh
+++ updateWiki.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cp  ~/Downloads/wiki/tiddly_wiki.html /Users/yonghao.hu/mycode/atlas-comstock.github.io/wiki/index.html ; 
+cp  ~/mycode/dev_configs/personal_notes/tiddly_wiki.html /Users/yonghao.hu/mycode/atlas-comstock.github.io/wiki/index.html ; 
 cd /Users/yonghao.hu/mycode/atlas-comstock.github.io/ || return; git add -u ; git cmm "new wiki" ; git pull  origin master -r && git push origin master
