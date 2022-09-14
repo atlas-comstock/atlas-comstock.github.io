78e2187c6b471d88cc970d3e1acd7298ae29a4bf
YongHao Hu <yonghao.hu@grabtaxi.com>
2022-09-15 00:34:04 +0800

    Pikpak

---
+++ pikpak/README.md
@@ -0,0 +1,18 @@
+# PikPak 个人网页版
+
+## 官方地址
+
+ * [PikPak官网](https://mypikpak.com)
+ * [PikPak官方网页版](https://drive.mypikpak.com/)
+
+## Demo
+ * [Demo](https://tjsky.github.io/pikpak/)
+
+## 安装教程
+  * [教程](https://www.tjsky.net/?p=201)
+
+## 对原版代码的修改
+ * 增加对IDM下载功能的引导
+ * 修正反代代码，支持下载反代（感谢小樱提供修正代码）
+ * 增加多个反代域名
+ * Demo站内置半透二次元背景图样式
---
+++ pikpak/src/config/index.ts
@@ -0,0 +1,14 @@
+export const proxy = [
+  'https://diii.tk',
+  'https://api-pikpak.go-back.cf',
+  'https://api-pikpak.tjsky.cf',
+  'https://api-pikpak.tjsky-1.cf',
+  'https://pikpak.he13797739994.ga',
+  'https://pikpak.hdddd.me',
+  'https://pan.yooyi.tk',
+  'https://pikpak.nsso.eu.org',
+  'https://pikpak-web.wnbh.cf',
+  'https://pk.pikpakz.top',
+]
+
+export const version = '1.23.0'
